9df01f71385279ba2a5c3a9c77ff91f326ae4f5f
georgegoldman <georgegoldmanjohn.o@gmail.com>
2025-03-29 20:02:24 +0100

    fix rocket binding for render

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 #[macro_use]
 extern crate rocket;
 
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use models::user;
-use rocket::routes;
+use rocket::{custom, routes};
 mod db;
 mod routes;
 mod models;
 
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Ninja!")
+}
+
+
 #[launch]
 async fn rocket() -> _ {
+    use std::env;
     let user_db = db::connect::<models::User>().await;
     let event_db = db::connect::<models::Event>().await;
     let application_db = db::connect::<models::Application>().await;
 
-    rocket::build()
+    let port = env::var("PORT")
+    .unwrap_or_else(|_| "8000".to_string() )
+    .parse::<u16>()
+    .expect("Invalid PORT number");
+
+    
+
+    rocket::custom(
+        rocket::Config {
+            address: "0.0.0.0".parse().unwrap(),
+            port,
+            ..rocket::Config::default()
+        }
+    )
     .manage(user_db)
     .manage(event_db)
     .manage(application_db)
